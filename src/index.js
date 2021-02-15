332b84de213206c340e267f2a5fe9f1989447b3f
Taras <taras369ky@gmail.com>
2021-02-15 16:09:25 +0200

    add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
-exports.min = function min (array) {
-  return 0;
-}
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    if (!array || array.length === 0  ){
+        return 0;
+    } else {
+        return Math.min( ...array );
+    }
+}; 
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0  ){
+        return 0;
+    } else {
+        return Math.max( ...array );
+    }
+};
+
+exports.avg = function avg(array) {
+    if (!array || array.length === 0  ){
+        return 0;
+    } else {
+        return array.reduce((a,b) => a + b, 0) / array.length;
+    }
+};
+
